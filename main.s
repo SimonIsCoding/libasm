29f99cb1df83f49ea66793e7aca37bf4f15fdc9f
SimonIsCoding <simon.archambault@hotmail.fr>
2026-03-10 14:06:29 +0800

    cleaning main

--- main.s
+++ main.s
@@ -6,41 +6,6 @@ extern  SYM(ft_strcmp)
 global  SYM(main)
 default rel
 
-;main for ft_strlen:
-;SYM(main):
-;	lea rdi, [message]
-;	call SYM(ft_strlen)
-;
-;	mov rsi, rax
-;	lea rdi, [format]
-;	xor rax, rax
-;	call SYM(printf)
-;
-;	xor rax, rax
-;	ret
-;
-;section .data
-;
-;message: db "This is an incredible test", 0
-;format:	db "Result: %d", 10, 0
-
-;main for strcpy:
-;SYM(main):
-;	lea rdi, [dest]
-;	lea rsi, [src]
-;	call _ft_strcpy
-;
-;	mov rdi, rax
-;	call SYM(printf)
-;	xor rax, rax
-;	ret
-;
-;section .data
-;
-;dest: db "empty", 0
-;src: db "this is a test", 0
-
-;main for strcmp:
 SYM(main):
 	lea rdi, [message]
 	call SYM(ft_strlen)
