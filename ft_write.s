703ba265e19c1068c94434ea389f040f375ee832
SimonIsCoding <simon.archambault@hotmail.fr>
2026-03-11 12:25:50 +0800

    It should work on MacOS & on Linux

--- ft_write.s
+++ ft_write.s
@@ -1,20 +1,33 @@
 %include "asm_header.inc"
 global SYM(ft_write)
-extern ___error
+%ifdef __APPLE__
+	extern	___error
+%else
+	extern	__errno_location
+%endif
 default rel
-
-;ssize_t		write(int fildes, const void *buf, size_t nbyte);
 
 section .text
 SYM(ft_write):
 	mov		rax, SYS_WRITE
 	syscall
-	jc		.error
+	%ifdef __APPLE__
+		jc		.error
+	%else
+		test	rax, rax
+		js		.error
+	%endif
 	ret
 
 .error:
-	push	rax
-	call	___error
+	%ifdef __APPLE__
+		push	rax
+		call	___error
+	%else
+		neg		rax
+		push	rax
+		call	__errno_location WRT ..plt
+	%endif
 	pop		rcx
 	mov     dword [rax], ecx
 	mov		rax, -1
